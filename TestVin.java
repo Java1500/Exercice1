c165765670ccb3aac47273bc6a377818922ed0d0
Java1500 <97265679+Java1500@users.noreply.github.com>
2022-06-24 22:18:04 -0400

    pas fini

--- TestVin.java
+++ TestVin.java
@@ -6,12 +6,14 @@ public class TestVin {
 
     public static void main(String args[]) {
 
-                
-            	Vin blanc = new Vin (("MiamMiam"),("Espagne"),(8.95));
+		
+        
+        Vin b = new Vin (("MiamMiam"),1,("Espagne"),(8.95));
           
-                Vin rouge = new Vin (("Délicieux"),("France"),(14.50));
+        Vin r = new Vin (("Délicieux"),("France"),(14.50));
               
-                Vin rose = new Vin (("Mystère"),("Californie"),(10.00));
+        Vin s = new Vin (("Mystère"),3,("Californie"),(10.00));
+
                
 		
 		JTextArea affichage = new JTextArea();
@@ -19,19 +21,35 @@ public class TestVin {
 		 	 
 		 affichage.append("Voici les " + Vin.nbVin + " vins\n");
 		 affichage.append("Le prix total des vins est de " + Vin.totalPrix + "$.\n");
-		 affichage.append(blanc.toString() + "\n");
-		 affichage.append(rouge.toString() + "\n");
-		 affichage.append(rose.toString() + "\n");
+		 affichage.append(b.toString() + "\n");
+		 affichage.append(r.toString() + "\n");
+		 affichage.append(s.toString() + "\n");
+		
 		 
 		 
 		 affichage.append("\n");
-		 
+
+		 b.setPrix(b.getPrix()+2);
+		 r.setPrix(23.00);
+		 r.setOrigine("Italie");
+		 s.setNom("Vino verde");
+		 /*s.setType();*/
+		 s.setOrigine(b.getOrigine());
+		 Vin n = new Vin (("L'érablière"),("Québec"),(15.00));
+
+
+		 affichage.append("Voici les " + Vin.nbVin + " vins\n");
+		 affichage.append("Le prix total des vins est de " + Vin.totalPrix + "$.\n");
+		 affichage.append(b.toString() + "\n");
+		 affichage.append(r.toString() + "\n");
+		 affichage.append(s.toString() + "\n");
+		 affichage.append(n.toString() + "\n");	 
 
 			
 			JOptionPane.showMessageDialog(null, affichage ,
 					"Résultats des vins", JOptionPane.PLAIN_MESSAGE);
 
-
+					
 
     }
 
--- Vin.java
+++ Vin.java
@@ -1,9 +1,9 @@
 package Exercice1;
 public class Vin {
 
-	final static int BLANC = 1;
-	 final static int ROUGE = 2;
-	 final static int ROSE = 3;
+	public static final  int BLANC = 1;
+	public static final  int ROUGE = 2;
+	public  static final  int ROSE = 3;
 
     private String nom;
     private int type;
@@ -15,19 +15,43 @@ public class Vin {
     public static double totalPrix = 0;
  
 
-    public Vin(String nom, String origine, double prix) {
+    public Vin(String nom,int type, String origine, double prix) {
         this.nom = nom;
+        this.type = type;
         this.origine = origine;
         this.prix = prix;
-        ++nbVin;
+        nbVin++;
+        totalPrix += prix;
+    }
+
+    public Vin(String nom, String origine, double prix) {
+        this.nom = nom;
+        this.type = ROUGE;
+        this.origine = origine;
+        this.prix = prix;
+        nbVin++;
         totalPrix += prix;
     }
     
     public Vin() {
     }
-   
+
+    
+    public static int getBLANC() {
+        return BLANC;
+    }
+
+    public static int getROUGE() {
+        return ROUGE;
+    }
+
+    public static int getROSE() {
+        return ROSE;
+    }
+
+
     public String getNom() {
-        return nom;
+        return this.nom;
     }
 
     public void setNom(String nom) {
@@ -35,35 +59,48 @@ public class Vin {
     }
 
     public int getType() {
-        return type;
+        return this.type;
     }
 
     public void setType(int type) {
-        this.type = type;
+        
+            this.type = type;
     }
 
     public String getOrigine() {
-        return origine;
+        return this.origine;
     }
 
     public void setOrigine(String origine) {
         this.origine = origine;
     }
 
-    public double getprix() {
-        return prix;
+    public double getPrix() {
+        return this.prix;
     }
 
     public void setPrix(double prix) {
         this.prix = prix;
     }
     
+    public String type_vin(int type){
+
+        if (type == BLANC){
+            return "blanc";
+        }
+        else if (type == ROUGE){
+            return "rouge";
+        }
+        else {
+            return "rosé";
+            
+        } 
+
+    }
 
 	@Override
 	public String toString() {
-		return  "\t" + nom + " est un vin "+" de "+ this.origine + " et son prix est de : " + String.format("%.2f $",this.prix );
-	
-	
+		return  "\t" + this.nom + " est un vin " + type_vin(this.type) +" de "+ this.origine + " et son prix est de : " + String.format("%.2f $",this.prix );
 
                 
     }
